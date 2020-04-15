bfa26e5a9d241ee22ac6e708b0ae12e4b219e074
Ray Kao <ray@peopleandcode.com>
2020-04-15 10:22:19 -0400

    refactoring apps to use remote git repo for modules

--- modules.tf
+++ modules.tf
@@ -1,8 +1,8 @@
 module "aks" {
-  source = "./aks-priv"
+  source = "git::git@github.com:raykao/az-tf-modules.git//aks"
 
-  cluster_name            = "${local.prefix}-privaks"
-  resource_group_name     = "democorpprivaks"
+  cluster_name            = "${local.prefix}-privaks001"
+  resource_group_name     = "${local.prefix}-privaks001"
   location                = azurerm_virtual_network.hub.location
   subnet_id               = azurerm_subnet.aksPrivCluster.id
   load_balancer_subnet_id = azurerm_subnet.loadBalancers.id
@@ -10,26 +10,26 @@ module "aks" {
 }
 
 module "jumpbox001" {
-  source = "./jumpbox"
+  source = "git::git@github.com:raykao/az-tf-modules.git//jumpbox"
 
+  server_name               = "${local.prefix}-jumpbox001"
   resource_group_name       = "${local.prefix}-jumpbox001"
   location                  = azurerm_virtual_network.hub.location
-  server_name               = "${local.prefix}-jumpbox001"
   subnet_id                 = azurerm_subnet.default.id
   public_ip_prefix_id       = azurerm_public_ip_prefix.hub.id
 }
 
-module "consul" {
-  source = "./consul"
+// module "consul" {
+//   source = "./consul"
 
-  resource_group_name = "${local.prefix}-consulmasters"
-  location            = azurerm_virtual_network.hub.location
-  subnet_id           = azurerm_subnet.consulVaultMasters.id
-  vmss_name           = "${local.prefix}-consulMasters"
-}
+//   resource_group_name = "${local.prefix}-consulmasters"
+//   location            = azurerm_virtual_network.hub.location
+//   subnet_id           = azurerm_subnet.consulVaultMasters.id
+//   vmss_name           = "${local.prefix}-consulMasters"
+// }
 
 module "aksPub" {
-  source = "./aks-priv"
+  source = "git::git@github.com:raykao/az-tf-modules.git//aks"
 
   cluster_name            = "${local.prefix}-pubaks"
   resource_group_name     = "${local.prefix}-pubaks"
